03725e2dd2700176d743a4d54db6fbe796a32952
suyash0103 <suyashghuge@gmail.com>
2018-03-16 14:32:33 +0530

    face detection xml

--- app/src/main/java/com/example/dhp/codefundo/MainActivity.java
+++ app/src/main/java/com/example/dhp/codefundo/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.dhp.codefundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.io.*;
+import android.app.*;
+import android.content.*;
+import android.net.*;
+import android.os.*;
+import android.view.*;
+import android.graphics.*;
+import android.widget.*;
+import android.provider.*;
 
 public class MainActivity extends AppCompatActivity {
 
